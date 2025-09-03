815be8b52178ea4c530e521fe1a6b2444b8d7c11
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-09-03 12:58:08 +0100

    setup clerk with convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "@/components/convex-client-provider";
+import ConvexClientProvider from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ClerkProvider>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import { SignInButton, useAuth, UserButton, useUser } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated, useConvexAuth } from "convex/react";
 
 export default function Home() {
-  return <div className="flex min-h-screen items-center justify-center">Home Page</div>;
+  const user = useUser().user;
+  return (
+    <>
+      <Authenticated>
+        <UserButton />
+        <div className="flex flex-col min-h-screen items-center justify-center">
+          <h1>Home Page</h1>
+          <p>Welcome, {user?.username || "User"}!</p>
+        </div>
+        ;
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+    </>
+  );
 }
